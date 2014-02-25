c0966fcf12c5d365ebb58e4dcd44e87d422d2dc2
yating zhang <yating@yating-OptiPlex-7010.(none)>
2014-02-25 20:33:32 +0800

    testsignal.c

--- experiments/testsignal.c
+++ experiments/testsignal.c
@@ -1,2 +1,18 @@
 #include<stdio.h>
+#include<signal.h>
 
+void handler(int signo, siginfo_t *info, void *context){
+	printf("address is %p\n", info->si_addr);
+	exit(0);
+}
+
+int main(){
+	struct sigaction act;
+	act.sa_handler = (void (*)(int))handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_SIGINFO;
+	sigaction(SIGSEGV, &act, NULL);
+	int *a = 1;
+	*a = 2;
+	printf("end of main\n");
+}
