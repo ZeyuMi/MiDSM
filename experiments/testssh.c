246c711e290c6dcd81e735567f31c3f8e5a97ab5
yzmizeyu <yzmizeyu@gmail.com>
2014-02-25 13:52:55 +0800

    modify testssh.c

--- experiments/testssh.c
+++ experiments/testssh.c
@@ -2,5 +2,5 @@
 #include<stdlib.h>
 
 int main(){
-	system("ssh -f yating@192.168.48.42 ./home/yating/Desktop/MiDSM/experiments/a.out");
+	system("ssh -fX yating@192.168.48.42 /home/yating/Desktop/MiDSM/experiments/a.out");
 }
