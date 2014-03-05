60222171e1f22f2b156cb19b0a100ec1ea05e16c
yzmizeyu <yzmizeyu@gmail.com>
2014-03-05 13:58:45 +0800

    syn test

--- tests/syn-test-host1.c
+++ tests/syn-test-host1.c
@@ -1,26 +1,28 @@
 #include "../src/net.h"
 #include "../src/syn.h"
+#include "../src/init.h"
 #include <string.h>
 #include <stdio.h>
 #include <netdb.h>
 #include <sys/socket.h>
 
 
-int myhostid;
-int hostnum;
-host_t hosts[MAX_HOST_NUM];
+//int myhostid;
+//int hostnum;
+//host_t hosts[MAX_HOST_NUM];
 
-int main(){
-	myhostid = 0;
-	strcpy(hosts[0].address, "192.168.48.42");
-	strcpy(hosts[0].username, "yating");
-	strcpy(hosts[1].address, "192.168.48.40");
-	strcpy(hosts[1].username, "zeyu");
-	hostnum = 2;
+int main(int argc, char **argv){
+	//myhostid = 0;
+	//strcpy(hosts[0].address, "192.168.48.42");
+	//strcpy(hosts[0].username, "yating");
+	//strcpy(hosts[1].address, "192.168.48.40");
+	//strcpy(hosts[1].username, "zeyu");
+	//hostnum = 2;
 
 
-	initnet();
-	initsyn();
+	//initnet();
+	//initsyn();
+	mi_init(argc, argv);
 	
 	printf("enter barrier\n");
 	mi_barrier();
