7976aa867243147a680d5924cb17499280bb88f7
yzmizeyu <yzmizeyu@gmail.com>
2014-03-12 14:46:42 +0800

    mem test

---
+++ tests/mem-test-host2.c
@@ -0,0 +1,36 @@
+#include "../src/net.h"
+#include "../src/syn.h"
+#include "../src/init.h"
+#include "../src/mem.h"
+#include <string.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+
+
+int main(int argc, char **argv){
+	mi_init(argc, argv);
+	
+	printf("enter barrier\n");
+	mi_barrier();
+	printf("exit barrier\n");
+
+	int i, j, result;
+	result = 0;
+	int *result = (int *)mi_alloc(sizeof(int));
+	printf("enter barrier\n");
+	mi_barrier();
+	printf("exit barrier\n");
+
+	for(i = 0; i < 10; i++){
+		mi_lock(0);
+		*result = *result + 1;
+		mi_unlock(0);
+	}
+	
+	
+	printf("enter barrier\n");
+	mi_barrier();
+	printf("exit barrier\n");
+	printf("result = %d\n",result);
+}
