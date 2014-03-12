a0eb90a4fef0e9f828a028b060c01ca153255322
yzmizeyu <yzmizeyu@gmail.com>
2014-03-12 15:46:13 +0800

    mem test

--- tests/mem-test-host1.c
+++ tests/mem-test-host1.c
@@ -11,28 +11,28 @@
 int main(int argc, char **argv){
 	mi_init(argc, argv);
 	
-	printf("enter barrier\n");
-	mi_barrier();
-	printf("exit barrier\n");
-
-	int i, j;
-	//int *result = (int *)mi_alloc(sizeof(int));
-	mi_lock(0);
-	//*result = 0;
-	mi_unlock(0);
-	printf("enter barrier\n");
-	mi_barrier();
-	printf("exit barrier\n");
-
-	for(i = 0; i < 10; i++){
-		mi_lock(0);
-	//	*result = *result + 1;
-		mi_unlock(0);
-	}
-	
-	
-	printf("enter barrier\n");
-	mi_barrier();
-	printf("exit barrier\n");
-	//printf("result = %d\n", *result);
+//	printf("enter barrier\n");
+//	mi_barrier();
+//	printf("exit barrier\n");
+//
+//	int i, j;
+//	int *result = (int *)mi_alloc(sizeof(int));
+//	mi_lock(0);
+//	*result = 0;
+//	mi_unlock(0);
+//	printf("enter barrier\n");
+//	mi_barrier();
+//	printf("exit barrier\n");
+//
+//	for(i = 0; i < 10; i++){
+//		mi_lock(0);
+//		*result = *result + 1;
+//		mi_unlock(0);
+//	}
+//	
+//	
+//	printf("enter barrier\n");
+//	mi_barrier();
+//	printf("exit barrier\n");
+//	printf("result = %d\n", *result);
 }
--- tests/mem-test-host2.c
+++ tests/mem-test-host2.c
@@ -12,25 +12,25 @@ int main(int argc, char **argv){
 	printf("hello,world from host2\n");
 	mi_init(argc, argv);
 	
-	printf("enter barrier\n");
-	mi_barrier();
-	printf("exit barrier\n");
-
-	int i, j;
+//	printf("enter barrier\n");
+//	mi_barrier();
+//	printf("exit barrier\n");
+//
+//	int i, j;
 //	int *result = (int *)mi_alloc(sizeof(int));
-	printf("enter barrier\n");
-	mi_barrier();
-	printf("exit barrier\n");
-
-	for(i = 0; i < 10; i++){
-		mi_lock(0);
+//	printf("enter barrier\n");
+//	mi_barrier();
+//	printf("exit barrier\n");
+//
+//	for(i = 0; i < 10; i++){
+//		mi_lock(0);
 //		*result = *result + 1;
-		mi_unlock(0);
-	}
-	
-	
-	printf("enter barrier\n");
-	mi_barrier();
-	printf("exit barrier\n");
+//		mi_unlock(0);
+//	}
+//	
+//	
+//	printf("enter barrier\n");
+//	mi_barrier();
+//	printf("exit barrier\n");
 //	printf("result = %d\n", *result);
 }
